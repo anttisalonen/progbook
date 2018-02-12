8278a1fe19ca81f744a1a063c2f8204cf4de03b2
Antti Salonen <ajsalonen@gmail.com>
2018-02-13 00:19:14 +0100

    add guessing app

---
+++ guess/guess.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+import redis
+
+import datetime
+import json
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/guess_start/")
+def guess_start():
+    return render_template('guess_start.html')
+
+@app.route("/guess/", methods=['GET'])
+def guess():
+    max_value = request.args.get('max_value', 25)
+    user = request.args.get('user', 'User')
+    return render_template('guess3.html', max_value=max_value, user=user)
+
+@app.route("/guess/hiscore", methods=['POST'])
+def hiscore():
+    data = request.get_json()
+    return get_hiscore(data['max_value'])
+
+def get_hiscore(key):
+    stored_data = r.lrange(key, 0, -1)
+    hiscore = [json.loads(s) for s in stored_data]
+    hiscore.sort(key=lambda x: x['guesses'])
+    return json.dumps(hiscore)
+
+@app.route("/guess/finished", methods=['POST'])
+def finished():
+    data = request.get_json()
+    curr_date = str(datetime.datetime.now())
+    r.lpush(str(data['max_value']), json.dumps({'guesses': data['guesses'], 'date': curr_date, 'user': data['user']}))
+    return get_hiscore(data['max_value'])
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+@app.route("/purge/")
+def purge():
+    r.flushdb()
+    return 'purged'
